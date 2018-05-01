73100d26e2d5cfbfc5a5f56de53713f996b16396
henrygarciaospina <henrygarcia0309@gmail.com>
2018-05-01 12:15:18 -0500

    Sesión 7 Vídeo 54 : Añadir contacto – vista y controller.

--- backendninja/src/main/java/com/udemy/controller/ContactController.java
+++ backendninja/src/main/java/com/udemy/controller/ContactController.java
@@ -1,18 +1,43 @@
 package com.udemy.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.udemy.constant.ViewConstant;
+import com.udemy.model.ContactModel;
 
 @Controller
 @RequestMapping("/contacts")
 public class ContactController {
 	
+	private static final Log LOGGER =  LogFactory.getLog(ContactController.class);
+	
+	@GetMapping("/cancel")
+	public String cancel() {
+		return ViewConstant.CONTACTS;
+		
+	}
+	
 	@GetMapping("/contactform")
-	private String redirectContactForm () {
+	private String redirectContactForm (Model model) {
+		model.addAttribute("contactModel", new ContactModel());
 		return ViewConstant.CONTACT_FORM;
+		
+	}
+	
+	@PostMapping("/addcontact")
+	public String addContact (@ModelAttribute(name="contactModel") ContactModel contactModel, 
+			Model model) {
+		LOGGER.info("METHOD: addContact() -- PARAMS: error =" + contactModel.toString());
+		model.addAttribute("result", 1);
+		return ViewConstant.CONTACTS;
 		
 	}
 	
